eb6ec1479c20b10ccfe76119147e218adbadcf07
Iskra0332 <iskra0332@gmx.com>
2018-12-04 20:50:23 -0600

    Changed CeasarsCipher to CeasarCipher

--- src/database/Help.java
+++ src/database/Help.java
@@ -4,13 +4,14 @@ public class Help {
 
     public static void welcomeMessage(){
         System.out.println("Welcome to the CeasarCiph program. Enter the command for what you would like to do. Type 'help' for a list of commands and information on the capabilities of CeaserCiph.");
+        System.out.println();
     }
     public static void help() {
         System.out.println("CeasarsCiph is a simple text encryption and decryption program. With the following commands you can encrypt or decrypt a message with the default encryption of +3 to the char value.");
         System.out.println();
         System.out.println("Below is a list of available commands. Type the command followed by help for more help with each command. Ex: 'Encrypt help'.");
         System.out.println();
-        System.out.println("Encrypt" + "\n" + "Decrypt" + "\n" + "SetPassphrase");
+        System.out.println("Help" + "\n" + "CeasarCipher" + "\n" + "");
         System.out.println();
     }
 }
