071aa50534944d8f4b15b4497249cf838b148a48
Iskra0332 <iskra0332@gmx.com>
2018-12-04 21:28:25 -0600

    Added to Help.java and added some documentation to CeasarCipher. Will pick up from here tomrrow.

--- src/database/CeasarCipher.java
+++ src/database/CeasarCipher.java
@@ -6,11 +6,15 @@ import ui.SaideesCiph;
 
 public class CeasarCipher {
 
+    //variables for the constructor and the encryption and decryption key. Default is set to 3 but there are methods to customize the key.
+
     private String ccFunction;
     private String ccParameter;
     private String ccHelp;
     private static int encryDef = 3;
     private static int decryDef = 3;
+
+    //Constructor sets the variables for the class. 'ccFunction' is Encrypt or Decrypt, 'ccParameter' will be the key which is 3 by default, 'ccHelp' is used for triggering help.
 
     public CeasarCipher(String ccFunction, String ccParameter, String ccHelp) {
         this.ccFunction = ccFunction;
@@ -18,13 +22,19 @@ public class CeasarCipher {
         this.ccHelp = ccHelp;
     }
 
+    //Class is ran from here, this method should execute anything in the class needed from the command. The if statment sorts out the command and executes the methods as needed.
+
     public void runCC() {
 
         System.out.println();
         System.out.println("Input text. Letters and spaces only.");
         System.out.println();
 
+        //This is where text to be encrypted or decrypted is put in.
+
         String input = ui.SaideesCiph.sc.nextLine();
+
+        // if statment that uses the variables ccFunction and ccParameter to determine what to do to the text.
 
         if (this.ccParameter != null) {
             int ccParameterInt = Integer.parseInt(this.ccParameter);
