b7c5411be739706f85054cee02d4a331a2f2bfd9
Iskra0332 <iskra0332@gmx.com>
2019-06-14 16:02:40 -0500

    Added Test class

---
+++ src/test/java/io/limasecurityworks/IrohaTest.java
@@ -0,0 +1,9 @@
+package io.limasecurityworks;
+
+public class IrohaTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Lima-Iroha testing grounds");
+    }
+}
