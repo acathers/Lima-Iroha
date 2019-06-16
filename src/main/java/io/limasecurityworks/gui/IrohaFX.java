9f9787fee5d4706385f3c3fda39870dba978aeae
Iskra0332 <iskra0332@gmx.com>
2019-06-16 10:27:54 -0500

    Gui work

--- src/main/java/io/limasecurityworks/gui/IrohaFX.java
+++ src/main/java/io/limasecurityworks/gui/IrohaFX.java
@@ -16,6 +16,7 @@ import io.limasecurityworks.ui.*;
 public class IrohaFX extends Application {
 
     Stage window;
+    Double menuButtons = 175.0;
 
     public static void main(String[] args) {
         launch(args);
@@ -28,14 +29,15 @@ public class IrohaFX extends Application {
 
         //GridPane with 10px padding around edge
         GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10,10,10,10));
-        grid.setVgap(8);
-        grid.setHgap(10);
+        grid.setPadding(new Insets(30, 10, 10, 0));
+        grid.setVgap(0);
+        grid.setHgap(0);
 
         //Name Label - constrains use (child, column, row)
-        Label nameLabel = new Label("Username;");
+        Label nameLabel = new Label("Iroha");
         GridPane.setConstraints(nameLabel, 0, 0);
 
+        /*
         //Name Input
         TextField nameInput = new TextField("Bucky");
         GridPane.setConstraints(nameInput, 1, 0);
@@ -49,15 +51,43 @@ public class IrohaFX extends Application {
         passInput.setPromptText("password");
         GridPane.setConstraints(passInput, 1, 1);
 
-        //Login
-        Button loginButton = new Button("Log In");
-        GridPane.setConstraints(loginButton, 1, 2);
+        */
+
+        //Dashboard button
+        Button dashButton = new Button("Dash Board");
+        GridPane.setConstraints(dashButton, 0, 1);
+        dashButton.setPrefWidth(menuButtons);
+        dashButton.setStyle("-fx-background-radius: 0");
+
+        //Ciphers button
+        Button ciphersButton = new Button("Ciphers");
+        GridPane.setConstraints(ciphersButton, 0, 2);
+        ciphersButton.setPrefWidth(menuButtons);
+        ciphersButton.setStyle("-fx-background-radius: 0");
+
+        //Settings button
+        Button settingsButton = new Button("Settings");
+        GridPane.setConstraints(settingsButton, 0, 3);
+        settingsButton.setPrefWidth(menuButtons);
+        settingsButton.setStyle("-fx-background-radius: 0");
+
+        //Help button
+        Button helpButton = new Button("Help");
+        GridPane.setConstraints(helpButton, 0, 4);
+        helpButton.setPrefWidth(menuButtons);
+        helpButton.setStyle("-fx-background-radius: 0");
+
+        //About button
+        Button aboutButton = new Button("About");
+        GridPane.setConstraints(aboutButton, 0, 5);
+        aboutButton.setPrefWidth(menuButtons);
+        aboutButton.setStyle("-fx-background-radius: 0");
 
         //Add everything to grid
-        grid.getChildren().addAll(nameLabel, nameInput, passLabel, passInput, loginButton);
+        grid.getChildren().addAll(nameLabel, dashButton, ciphersButton, settingsButton, helpButton, aboutButton);
 
-        Scene scene = new Scene(grid, 300, 200);
-        scene.getStylesheets().add("resources/FlatRed.css");
+        Scene scene = new Scene(grid, 800, 600);
+        scene.getStylesheets().add("FlatRed.css");
         window.setScene(scene);
         window.show();
 
